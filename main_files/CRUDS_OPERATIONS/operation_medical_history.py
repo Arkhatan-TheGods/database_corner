1608b16722cf64860fe321a21b63dc96b84ec854
Arkhatan-TheGods <pauloevertron@outlook.com>
2023-02-07 14:45:48 -0300

    movendo funções para módulo correto

--- main_files/CRUDS_OPERATIONS/operation_medical_history.py
+++ main_files/CRUDS_OPERATIONS/operation_medical_history.py
@@ -1,10 +1,41 @@
+from sqlite3 import Cursor
 import CRUDS_QUERYS.query_medical_history as history_query
+import menu.menu as menu 
 
-
-# insere valores no medical history
-def insert_medical_history():
+def insert_medical_history() -> tuple:
     id_paciente = int(input('informe o id do paciente: ').strip())
     doenca = input('informe a doença(s): ')
     alergia = input('informe a(s) alergia(a): ')
     medicacao = input('informe os medicamentos: ')
     return id_paciente,doenca,alergia,medicacao
+
+def find_medical_history(ID, cursor):
+    result = cursor.execute(medical_history.query_find_medical_history_by_id_patient()).fetchone()
+    if result:
+        print(result)
+    elif not result:
+        print('nenhum resultado.')
+    return result if result else None
+
+def medical_history(cursor):
+    while True:
+        consulta = menu.submenu_medical_history()
+
+        if consulta == '1':
+            name = input('informe o nome: ').strip()
+            result = find_patient_by_name(name, cursor)
+            
+
+        elif consulta == '2':
+            ID = int(input('informe o ID: ').strip())
+            
+        elif consulta == '3':
+            pass
+
+        elif consulta == '4':
+            pass
+
+        elif consulta == '5':
+            break
+        else:
+            print('opção inválida, tente novamente.')
--- main_files/CRUDS_OPERATIONS/operation_patient.py
+++ main_files/CRUDS_OPERATIONS/operation_patient.py
@@ -1,7 +1,6 @@
 from sqlite3 import Cursor
 import CRUDS_QUERYS.query_patient as patient_query
 import CRUDS_QUERYS.query_medical_history as medical_history
-import menu.menu as menu
 
 class Patient():
 
@@ -99,28 +98,3 @@ def submenu_find_or_update(cursor):
         elif consulta == '4':
             break
 
-def find_medical_history(ID, cursor):
-    result = cursor.execute(medical_history.query_find_medical_history_by_id_patient()).fetchone()
-    if result:
-        print(result)
-    elif not result:
-        print('nenhum resultado.')
-    return result if result else None
-
-def medical_history(cursor):
-    while True:
-        consulta = menu.submenu_medical_history()
-
-        if consulta == '1':
-            name = input('informe o nome: ').strip()
-            result = find_patient_by_name(name, cursor)
-            
-
-        elif consulta == '2':
-            ID = int(input('informe o ID: ').strip())
-            
-
-        elif consulta == '3':
-            break
-        else:
-            print('opção inválida, tente novamente.')
